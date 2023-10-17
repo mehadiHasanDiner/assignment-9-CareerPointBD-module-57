85f2ef56c7ff7f06f31c06d4c53488dc495b746a
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-10-18 00:18:58 +0600

    job show details page is completed

--- src/components/Pages/JobDetails.jsx
+++ src/components/Pages/JobDetails.jsx
@@ -1,7 +1,14 @@
 import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { JobsContext } from "../Layout/Main";
-import { getAppliedJob } from "../../utilities/fakeDB";
+import {
+  CurrencyDollarIcon,
+  CalendarDaysIcon,
+  PhoneIcon,
+  EnvelopeIcon,
+  MapPinIcon,
+  BuildingOfficeIcon,
+} from "@heroicons/react/24/solid";
 
 const JobDetails = () => {
   const { jobId } = useParams();
@@ -18,17 +25,80 @@ const JobDetails = () => {
 
   return (
     <div>
-      <h1 className="text-5xl text-center">{jobDetails.company_name}</h1>
-
-      <div>
-        <p>Job Description: {jobDetails.job_description}</p>
-        <p>Job Responsibility: {jobDetails.job_responsibility}</p>
-        <p>Educational Requirements:</p>
-        <p>{jobDetails.education}</p>
-        <p>Experiences:</p>
-        <p>{jobDetails.experience}</p>
+      <div className=" mx-16 my-16 grid grid-cols-1 md:grid-cols-4 gap-4">
+        <div className="mr-2 col-span-3">
+          <p className="py-4 text-justify">
+            {" "}
+            <span className="font-bold">Job Description:</span>{" "}
+            {jobDetails.job_description}
+          </p>
+          <p className="text-justify">
+            {" "}
+            <span className="font-bold">Job Responsibility:</span>{" "}
+            {jobDetails.job_responsibility}
+          </p>
+          <p className="py-4">
+            <span className="font-bold">Educational Requirements:</span>
+          </p>
+          <p>{jobDetails.education}</p>
+          <p className="py-4">
+            <span className="font-bold">Experiences:</span>
+          </p>
+          <p>{jobDetails.experience}</p>
+        </div>
+        <div className="">
+          <div className="bg-purple-100 rounded-lg py-6 px-3 mt-3">
+            <h3 className="text-xl font-bold">Job Details</h3>
+            <div className="divider my-2"></div>
+            <div className="flex mb-4">
+              <CurrencyDollarIcon className="h-5 w-5 text-purple-400" />
+              <p>
+                {" "}
+                <strong>Salary:</strong> {jobDetails.salary} (Per Month)
+              </p>
+            </div>
+            <div className="flex">
+              <CalendarDaysIcon className="h-5 w-5 text-purple-400" />
+              <p>
+                {" "}
+                <strong>Job Title:</strong> {jobDetails.job_title}{" "}
+              </p>
+            </div>
+            <h3 className="text-xl font-bold mt-8">Contact Information</h3>
+            <div className="divider my-2"></div>
+            <div className="flex mb-4">
+              <BuildingOfficeIcon className="h-5 w-5 text-purple-400" />
+              <p>
+                {" "}
+                <strong>Organization:</strong> {jobDetails.company_name}
+              </p>
+            </div>
+            <div className="flex mb-4">
+              <PhoneIcon className="h-5 w-5 text-purple-400" />
+              <p>
+                {" "}
+                <strong>Phone:</strong> {jobDetails.contact_phone}
+              </p>
+            </div>
+            <div className="flex mb-4">
+              <EnvelopeIcon className="h-5 w-5 text-purple-400" />
+              <p>
+                {" "}
+                <strong>Email:</strong> {jobDetails.contact_email}
+              </p>
+            </div>
+            <div className="flex">
+              <MapPinIcon className="h-5 w-5 text-purple-400" />
+              <p>
+                {" "}
+                <strong>Address:</strong> {jobDetails.location}, Dhaka,
+                Bangladesh
+              </p>
+            </div>
+          </div>
+          <button className="btn-hero mt-4 w-full">Apply Now</button>
+        </div>
       </div>
-      <div></div>
     </div>
   );
 };
